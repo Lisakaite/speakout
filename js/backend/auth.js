8e43ff752e75f5163cdfeb42d6321e24ff66fa17
latrondav <89303631+latrondav@users.noreply.github.com>
2023-03-23 21:43:54 +0300

    google signin implemented

--- js/backend/auth.js
+++ js/backend/auth.js
@@ -1,24 +1,6 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyANqN__2HjQ_9YC4Fv0cigIO35gcJ_7wI4",
-//   authDomain: "speak-up-a206d.firebaseapp.com",
-//   projectId: "speak-up-a206d",
-//   storageBucket: "speak-up-a206d.appspot.com",
-//   messagingSenderId: "563219607865",
-//   appId: "1:563219607865:web:39ea917f275642ab5bb8e8"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
 import { getFirestore, collection, doc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, signInWithPopup, signInWithRedirect, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, signInWithPopup, signInWithRedirect, getRedirectResult, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -33,7 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+const provider = new GoogleAuthProvider(app);
 
 const signup = document.getElementById('signup');
 if (signup) {
@@ -41,16 +23,18 @@ if (signup) {
         var firstname = ""
         var lastname = ""
         var email = document.getElementById('email').value;
+        var photoURL = ""
         var password = document.getElementById('password').value;
+
         
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 const user = userCredential.user;
 
                 setDoc(doc(db, 'Users', user.uid), {
-                    firstname: firstname,
-                    lastname: lastname,
-                    email: email
+                    name: firstname + " " + lastname,
+                    email: email,
+                    photoURL: photoURL
                 });
 
                 alert('User Created');
@@ -63,16 +47,67 @@ if (signup) {
     });
 }
 
-const googleSignInButton = document.getElementById('google-sign-in');
-if (googleSignInButton) {
+
+const login = document.getElementById('login');
+if (login) {
+    login.addEventListener('click', (e) => {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+
+                const userRef = doc(db, 'Users', user.uid);
+                updateDoc(userRef, {
+                    last_login: new Date().toString()
+                })
+                    .then(() => {
+                        alert("Login Successfull");
+                        window.location = "http://127.0.0.1:5500/index.html"
+                    })
+                    .catch((error) => {
+                        const errorMessage = error.message;
+                        alert(errorMessage);
+                    });
+
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                alert(errorMessage);
+            });
+    });
+}
+
+const googleSignInButton = document.getElementById('googlesignin');
+if (googleSignInButton) { 
     googleSignInButton.addEventListener('click', (e) => {
-        signInWithPopup(auth, provider)
+        signInWithRedirect(auth, provider);
+        getRedirectResult(auth)
             .then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
+                // This gives you a Google Access Token. You can use it to access Google APIs.
                 const credential = GoogleAuthProvider.credentialFromResult(result);
                 const token = credential.accessToken;
+
                 // The signed-in user info.
                 const user = result.user;
+
+                // Save user data to Firestore
+                // const usersCollection = collection(db, 'users');
+                // const userDocRef = doc(usersCollection, user.uid);
+                // setDoc(userDocRef, {
+                //     name: user.displayName,
+                //     email: user.email,
+                //     photoURL: user.photoURL
+                // });
+
+                setDoc(doc(db, 'Users', user.uid), {
+                    name: user.displayName,
+                    email: user.email,
+                    photoURL: user.photoURL
+                });
+
                 // IdP data available using getAdditionalUserInfo(result)
                 // ...
             }).catch((error) => {
@@ -85,6 +120,60 @@ if (googleSignInButton) {
                 const credential = GoogleAuthProvider.credentialFromError(error);
                 // ...
             });
+    });
+}
+
+
+// const googleSignInButton = document.getElementById('googlesignin');
+// if (googleSignInButton) {
+//     googleSignInButton.addEventListener('click', (e) => {
+//         signInWithPopup(auth, provider)
+//             .then((result) => {
+//                 // This gives you a Google Access Token. You can use it to access the Google API.
+//                 const credential = GoogleAuthProvider.credentialFromResult(result);
+//                 const token = credential.accessToken;
+//                 // The signed-in user info.
+//                 const user = result.user;
+//                 // IdP data available using getAdditionalUserInfo(result)
+//                 // ...
+//             }).catch((error) => {
+//                 // Handle Errors here.
+//                 const errorCode = error.code;
+//                 const errorMessage = error.message;
+//                 // The email of the user's account used.
+//                 const email = error.customData.email;
+//                 // The AuthCredential type that was used.
+//                 const credential = GoogleAuthProvider.credentialFromError(error);
+//                 // ...
+//             });
+//     });
+// }
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        // ...
+    } else {
+        // User is signed out
+        // ...
+    }
+});
+
+const logout = document.getElementById('logout');
+if (logout) {
+    logout.addEventListener('click', (e) => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            alert('Logged Out Successfully');
+            window.location = "http://127.0.0.1:5500/login.html";
+        }).catch((error) => {
+            // An error happened.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage);
+        });
     });
 }
 
